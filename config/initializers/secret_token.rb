05598b8eec71f28922394acfc91eb75b4724466b
Edgecase Pairing <pairing@edgecase.com>
2013-08-20 19:24:30 -0400

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AffiliateApp::Application.config.secret_token = 'fb725f7d4554de8efb41278d1abb86f97996cc863dcf79c5c7d1cf42125e3c063c7ee506a0ebd213704e6c8204c9d6142f258d5f91216186a25b627f0ee47d08'
